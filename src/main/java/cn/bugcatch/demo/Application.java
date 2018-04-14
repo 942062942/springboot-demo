c1526cf7d5d981f8b94b49c1e651d4bbc0615853
gao <942062942@qq.com>
2018-04-14 23:24:51 +0800

    update

--- src/main/java/cn/bugcatch/demo/Application.java
+++ src/main/java/cn/bugcatch/demo/Application.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Profile;
 @ComponentScan//不需要指定扫描路径，原因见下面注释
 //通常建议将应用的main类放到其他类所在包的顶层(root package)，并将@EnableAutoConfiguration注解到你的main类上，这样就隐式地定义了一个基础的包搜索路径
 @EnableAutoConfiguration
-@Profile("dev")//声明此配置类文件只有在dev Profile被激活时生效，被此注解标注的类相当于application-{profile}.properties属性文件(此处只是demo，实际使用应放在特定配置类文件中，比如jdbc配置信息)
+@Profile("dev")//此注解的作用与application-dev.properties属性文件类似，声明只有在dev Profile被激活时当前配置类才会生效(此处只是demo，实际使用应放在有意义的配置类文件中，比如jdbc配置信息)
 //@Configuration
 public class Application {
 	/**
--- src/main/java/cn/bugcatch/demo/web/BaseController.java
+++ src/main/java/cn/bugcatch/demo/web/BaseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BaseController {
 	@Value("${name}")
 	private String name;
